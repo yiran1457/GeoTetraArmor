d8c0d2cdf65b1870d401b0ce7b45b8edbc2d2a04
yiran1457 <3270897489@qq.com>
2026-06-03 16:23:26 +0800

    6.9.0-1.0.4: 添加损失耐久度的事件触发

--- src/main/java/ovo/yiran/geotetraarmor/core/mixins/PlayerMixin.java
+++ src/main/java/ovo/yiran/geotetraarmor/core/mixins/PlayerMixin.java
@@ -2,11 +2,11 @@ package ovo.yiran.geotetraarmor.core.mixins;
 
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ovo.yiran.geotetraarmor.items.ModularArmorItem;
+import se.mickelus.tetra.event.ModularItemDamageEvent;
 
 @Mixin(Player.class)
 public class PlayerMixin {
@@ -32,7 +33,9 @@ public class PlayerMixin {
             for (int i = 0; i < 4; i++) {
                 ItemStack itemstack = this.inventory.armor.get(i);
                 if ((!source.is(DamageTypeTags.IS_FIRE) || !itemstack.getItem().isFireResistant()) && itemstack.getItem() instanceof ModularArmorItem armorItem) {
-                    armorItem.applyDamage((int) amount,itemstack,this.inventory.player,i);
+                    ModularItemDamageEvent event = new ModularItemDamageEvent((LivingEntity) (Object)this,itemstack,(int)amount);
+                    MinecraftForge.EVENT_BUS.post(event);
+                    armorItem.applyDamage(event.getAmount(),itemstack,this.inventory.player,i);
                 }
             }
         }
